5205d27198a474160fe1cb47156142af859d09d7
souljavan <jcavanilles@gmail.com>
2021-10-30 14:23:36 +0200

    iteracion4

---
+++ Iteracion4.js
@@ -0,0 +1,37 @@
+//4.1
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const usernames=users.map(function(x){return(x.name)});
+console.log(usernames)
+
+//4.2
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+const userfiltered=users.map(function(x){
+    let temp = Object.assign({}, x);
+    if (temp.name.startsWith('A')){
+        temp.name='Anacleto';
+        
+    }
+    return(temp)
+    
+})
+console.log(userfiltered)
+
+//4.3
+
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
