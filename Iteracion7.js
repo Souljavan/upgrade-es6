adee13eb70e7fb267c038f7f0e0e871e6af48dfd
souljavan <jcavanilles@gmail.com>
2021-11-04 20:23:44 +0100

    Iteracion7

---
+++ Iteracion7.js
@@ -0,0 +1,46 @@
+//7.1
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+const examfilter=exams.reduce((acumulador,valoract)=>acumulador +valoract.score,0);
+console.log(examfilter)
+
+//7.2
+
+const examfilter72=exams.reduce(function(sum,valoract){
+    if (valoract.score >=5){ return sum+valoract.score}
+    return sum
+   
+},0);
+console.log(examfilter72)
+
+//7.3
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+const exammedia73=exams.reduce(function(sum,valoract){
+    if (valoract.score >=5){ return sum+valoract.score}
+    return sum/exams.length;
+   
+},0);
+console.log(exammedia73)
